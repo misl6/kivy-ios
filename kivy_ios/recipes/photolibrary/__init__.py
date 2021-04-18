62afce80d51ece9b071052895b230bb200280701
Mirko Galimberti <me@mirkogalimberti.com>
2021-04-18 10:00:26 +0200

    Rename MobileCoreServices to CoreServices

--- kivy_ios/recipes/photolibrary/__init__.py
+++ kivy_ios/recipes/photolibrary/__init__.py
@@ -6,7 +6,7 @@ class PhotoRecipe(CythonRecipe):
     url = "https://github.com/akshayaurora/photolibrary/archive/master.zip"
     library = "libphotolibrary.a"
     depends = ["python"]
-    pbx_frameworks = ["UIKit", "Photos", "MobileCoreServices"]
+    pbx_frameworks = ["UIKit", "Photos", "CoreServices"]
 
     def install(self):
         self.install_python_package(name="photolibrary.so", is_dir=False)
